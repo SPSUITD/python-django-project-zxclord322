edb491378e9a79ffde7dff31bbb66965056693e3
zxclord322 <79160247+zxclord322@users.noreply.github.com>
2021-05-18 02:53:13 +0300

    Add files via upload

--- Web/main/forms.py
+++ Web/main/forms.py
@@ -21,17 +21,17 @@ class FormCreate(ModelForm):
 class UserLogin(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(
         attrs={'type': 'Name', 'placeholder': 'Введите имя пользователя', 'class': 'formimput', }))
-    password = forms.CharField(label='Пороль', widget=forms.PasswordInput(
-        attrs={'type': 'Password', 'placeholder': 'Введите пороль', 'class': 'formimput', }))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={'type': 'Password', 'placeholder': 'Введите пароль', 'class': 'formimput', }))
 
 
 class UserRegister(UserCreationForm):
     username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(
         attrs={'type': 'Name', 'placeholder': 'Введите имя пользователя', 'class': 'formimput', }))
-    password1 = forms.CharField(label='Пороль', widget=forms.PasswordInput(
-        attrs={'type': 'password', 'placeholder': 'Введите пороль', 'class': 'formimput', }))
-    password2 = forms.CharField(label='Подтвердитье пороль', widget=forms.PasswordInput(
-        attrs={'type': 'password', 'placeholder': 'Подтвердитье пороль', 'class': 'formimput', }))
+    password1 = forms.CharField(label='Пароль', widget=forms.PasswordInput(
+        attrs={'type': 'password', 'placeholder': 'Введите пароль', 'class': 'formimput', }))
+    password2 = forms.CharField(label='Подтвердите пароль', widget=forms.PasswordInput(
+        attrs={'type': 'password', 'placeholder': 'Подтвердите пароль', 'class': 'formimput', }))
 
     class Meta:
         model = User
@@ -45,12 +45,12 @@ class UserRegister(UserCreationForm):
             }),
             'password1': forms.PasswordInput(attrs={
                 'type': 'password',
-                'placeholder': 'Введите пороль',
+                'placeholder': 'Введите пароль',
                 'class': 'formimput',
             }),
             'password2': forms.PasswordInput(attrs={
                 'type': 'password',
-                'placeholder': 'Подтвердитье пороль',
+                'placeholder': 'Подтвердите пароль',
                 'class': 'password',
             }),
         }
